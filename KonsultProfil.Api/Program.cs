3c9f67af8cfdd966de786e2158dc3ed3630df213
Samuel Brinkebäck <samuel.brinkeback@acecom.se>
2022-08-15 16:31:38 +0200

    Added CORS

--- KonsultProfil.Api/Program.cs
+++ KonsultProfil.Api/Program.cs
@@ -3,7 +3,20 @@ using KonsultProfil.Api.Models;
 using KonsultProfil.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:4200")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                      });
+});
 
 // Add services to the container.
 builder.Services.AddScoped<IConsultProfileRepository, ConsultProfileRepository>();
@@ -27,6 +40,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
